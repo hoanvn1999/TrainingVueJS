425dcd49f92e31de71b6719645fe503e95a0c430
Pham Le Hoan <hoanpham@hoan.pham>
2021-07-12 23:10:48 +0700

    Session 2: Loops

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 new Vue({
   el: '#app',
   data: {
-       app_name: 'To do list',
-       icon: '<i class="fa fa-calendar" aria-hidden="true"></i>'
+    app_name: 'To do list',
+    icon: '<i class="fa fa-calendar" aria-hidden="true"></i>',
+    tasks: [{ action: "Buy Flowers", image_link: "", done: false },
+            { action: "Get Shoes", image_link: "", done: false },
+            { action: "Collect Tickets", image_link: "", done: true },
+            { action: "Call Joe", image_link: "", done: false }]
   }
 })
