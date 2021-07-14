d3c19763cff67a669dfcf2d52e42ff09ed701274
Pham Le Hoan <hoanpham@hoan.pham>
2021-07-14 20:46:08 +0700

    Session 10: Components

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,25 @@
 <template>
   <div id="app">
-    
+    <navigation />
+    <general />
+    <provinces />
   </div>
 </template>
 
 <script>
-
+import General from './components/General.vue';
+import Navigation from './components/Navigation.vue';
+import Provinces from './components/Provinces.vue';
 export default {
   name: 'App',
   components: {
+    Navigation,
+    General,
+    Provinces
   }
 }
 </script>
+
 
 <style>
 </style>
---
+++ src/components/Navigation.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="row">    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navigation'
+}
+</script>
+
+<style scoped>
+</style>
